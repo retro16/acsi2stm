442e533904a56d6532a0b695f5d21943af061367
Jean-Matthieu Coulon <retro16@coulon-jm.fr>
2023-07-17 15:55:45 +0200

    4.2b: EmuTOS, boot sector fix, documentation update.

--- acsi2stm/acsi2stm.h
+++ acsi2stm/acsi2stm.h
@@ -22,7 +22,7 @@
 
 // acsi2stm global configuration
 
-#define ACSI2STM_VERSION "4.2a"
+#define ACSI2STM_VERSION "4.2b"
 
 // Set to 1 to enable debug output on the serial port
 #define ACSI_DEBUG 0
