3b49d2eee878eb949cb7f875032160a79b142f44
tin-nl <tin@tzi.de>
2025-05-18 20:09:59 +0200

    skip gemdrive mode if any modifier key (shift/ctrl/alt) is pressed at boot time

--- asm/GEMDRIVE/boot.s
+++ asm/GEMDRIVE/boot.s
@@ -29,7 +29,19 @@ acsiid	moveq	#$0e,d0                 ; Set command (patched by code)
 
 	include	syshook.s               ; Enter syshook mode
 
-load	st	flock.w                 ; Lock floppy controller
+load
+	; we'll give the user a chance to skip GEMDOS mode if anything goes awry (yes, I'm looking at you MSTE+16Mhz+cache)
+	move.w  #-1,-(sp)   			
+	move.w  #$B,-(sp)    			; KBshift
+	trap    #13          
+	addq.l  #4,sp
+	and.w   #$f,d0        			; check RShift/LShift/Ctrl/Alt
+	beq.s   .resumeload 			; continue if no modifier key pressed
+	; exit since the user asked us nicely
+	rts
+.resumeload	
+
+	st	flock.w                 ; Lock floppy controller
 
 	bsr.w	syshook.setdmaaddr      ; Reset DMA chip
 	move.w	#$0088,(a1)             ; Switch to command.
